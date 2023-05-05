72644a7baba9f8265f7f5bd0f15d8873da1792e3
jhon-stack <jgaitanaguilar@gmail.com>
2023-05-04 20:47:46 -0600

    Se agregan los pasos finales para la ejecucion de excepciones

---
+++ Excepciones/README.md
@@ -0,0 +1,22 @@
+# Descripción del programa
+
+En este directorio se documenta y ejecuta el uso de exceciones donde se divide la excepcion personalizada como clase en un header aparte llamado headerExceptions.hpp 
+
+## Requisitos previos
+
+Lista de los requisitos previos necesarios para poder compilar y ejecutar el programa:
+
+- Sistema operativo: Windows 10, Ubuntu 18.04, MacOS 13.3.1, etc.
+- Lenguaje de programación: C++
+- Versión del compilador: C++
+
+## Pasos necesarios para compilar el programa
+
+1. Descargar el código fuente de este repositorio en GitHub.
+2. Abrir permisos de ejecución.
+3. Ejecutar la línea: `make`
+
+## Pasos necesarios para ejecutar el programa
+
+1. Abrir una terminal en el directorio raíz del programa.
+2. Ejecutar el comando `./exceptions` para iniciar la aplicación.
--- Excepciones/exceptions.cpp
+++ Excepciones/exceptions.cpp
@@ -24,21 +24,60 @@ OTROS ACUERDOS EN EL SOFTWARE.
 */
 
 #include <iostream>
+#include <stdexcept>
+#include "headerExceptions.hpp"
 
-class MyException : public std::exception {
-public:
-  const char* what() const throw() {
-    return "My custom exception";
-  }
-};
 
 int main() {
-
+  int a,b;//-->a -> dividendo, b -> divisor
+  int c,d;//-->c -> dividendo, d -> divisor
+  //Exception generada por el usuario (propia)
+  //https://www.geeksforgeeks.org/handling-the-divide-by-zero-exception-in-c/
+  //bloque try catch 
   try {
     throw MyException();
+    //por referencia de std::excepcion
   } catch (std::exception& e) {
+    //Se genera la excepcion mientras se llaman al metodo de la clase myException
     std::cout << "Error: " << e.what() << std::endl;
   }
+  //excepciones de la bibioletca standart
+  
+  a = 100;
+  b = 0;
+  //bloque try-catch
+  if (b == 0){
+    throw std::invalid_argument("El divisor no puede ser 0");
+  }
+  try{
+    int cociente = a/b;
+    std::cout << "El resultado de la division es:" << cociente << std::endl;
+  }
+  catch(std::exception& e){
+    std::cout << "Error: " << e.what() << std::endl;
+  }
+  //excepcion en tieempo de ejecucion
+  //RuntimeExceptions are those exceptions which are checked at runtime.
+  c = 100;
+  d = 0;
+  try
+  {
+    
+    if (d == 0){
+      throw std::runtime_error("El divisor no puede ser 0");
+    }
+    int cociente_1 = c/d;
+    std::cout << "El resultado de la division es:" << cociente_1 << std::endl;
+  }
+  
+  catch(const std::exception& e)
+  {
+    std::cout << "Error: " << e.what() << std::endl;
+  }
+  
+  
 
-  return 0;
+  
+
+  return 0; 
 }
